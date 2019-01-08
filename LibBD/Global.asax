8b828de669c0bf7ab5f268a44241928a66758869
WssIDs <wssids49@gmail.com>
2019-01-08 20:20:13 +0300

    Add project files.

---
+++ LibBD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibBD.MvcApplication" Language="C#" %>
---
+++ LibBD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibBD.Startup))]
+namespace LibBD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
