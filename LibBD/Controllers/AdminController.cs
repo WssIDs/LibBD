e66b7d3ae52897f47121fc57b859c9161b1d91d5
WssIDs <wssids49@gmail.com>
2019-01-18 16:53:32 +0300

    Добавлены методы наследия

--- LibBD/Controllers/AdminController.cs
+++ LibBD/Controllers/AdminController.cs
@@ -10,11 +10,14 @@ namespace LibBD.Controllers
     [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
+        IRepository<Author> repositoryHeritage;
         IRepository<Author> repository;
+        //IRepository<Author> repository;
 
         public AdminController(IRepository<Author> repo)
         {
             repository = repo;
+            repositoryHeritage = repo;
         }
 
 
@@ -23,6 +26,110 @@ namespace LibBD.Controllers
         {
             return View(repository.GetAll());
         }
+
+        public ActionResult Heritage()
+        {
+            return View(repositoryHeritage.GetAll());
+        }
+
+        // GET: Admin/HeritageCreate
+        public ActionResult HeritageCreate()
+        {
+            var author = new Author();
+            author.CardYear = DateTime.Now.Year;
+            author.Year = DateTime.Now.Year;
+            return View(author);
+        }
+
+        // POST: Admin/HeritageCreate
+        [HttpPost]
+        public ActionResult HeritageCreate(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    repositoryHeritage.Create(author);
+                    return RedirectToAction("Heritage");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            else return View(author);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult HeritageEdit(int id)
+        {
+            return View(repositoryHeritage.Get(id));
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult HeritageEdit(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    repositoryHeritage.Update(author);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            else return View(author);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult HeritageDelete(int id)
+        {
+            return View(repositoryHeritage.Get(id));
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult HeritageDelete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                repositoryHeritage.Delete(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult HeritageDetails(int id)
+        {
+            return View(repositoryHeritage.Get(id));
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult HeritageDetails(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                repositoryHeritage.Get(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        /**************************************************/
 
         // GET: Admin/Create
         public ActionResult Create()
